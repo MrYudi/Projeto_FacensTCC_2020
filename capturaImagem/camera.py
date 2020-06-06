736d15c173299f259e5d824068cfee37412a7a77
MrYudi <36682691+MrYudi@users.noreply.github.com>
2020-06-06 17:58:45 -0300

    Update camera.py

--- capturaImagem/camera.py
+++ capturaImagem/camera.py
@@ -8,34 +8,50 @@ import os
 video = cv2.VideoCapture(0)
 path = 'capturaImagem/Dataset/'
 count = 0
-dicionario = {
+dicionario = { 255: 'NADA',
 65:'A',66:'B',67:'C',68:'D',69:'E',70:'F',
 71:'G',72:'H',73:'I',74:'J',75:'K',76:'L',
 77:'M',78:'N',79:'O',80:'P',81:'Q',82:'R',
 83:'S',84:'T',85:'U',86:'V',87:'W',88:'X',
 89:'Y',90:'Z'}
+limite_frame = 30
+count_frame = 30
+letra = k = 255
 
 while(1):
     _, frame = video.read()
-    cv2.imshow("Video", frame)
-   
+    
     k = cv2.waitKey(30) & 0xff # Escape
+
     if k == 27:
         break
-    elif (65 <= k and k <= 90) or (97 <= k and k <= 122):
 
-        if(97 <= k and k <= 122): #caso seja minusculo
-            k = k - 32
+    if (count_frame < limite_frame):
 
+        count_frame = 1 + count_frame
         count = 1 + count
-        nomeArquivo = dicionario[k] + str(count)+'.jpg'
-        caminho = path + dicionario[k]
+
+        nomeArquivo = dicionario[letra] + str(count)+'.jpg'
+        caminho = path + dicionario[letra]
 
         if not os.path.isdir(caminho): 
             os.mkdir(caminho) # Cria a pasta, caso nao exista
 
         cv2.imwrite(caminho +'/'+ nomeArquivo, frame) 
         print("Imagem: " + nomeArquivo)
+
+    else:
+        
+        if(65 <= k and k <= 90) or (97 <= k and k <= 122):
+            if(97 <= k and k <= 122): #caso seja minusculo
+                k = k - 32
+
+            letra = k
+            count_frame = 0
+
+    cv2.putText(frame,"Frame: "+str(count_frame),(0,50), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2,cv2.LINE_AA) 
+    cv2.putText(frame,"Letra: "+dicionario[letra],(0,100), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2,cv2.LINE_AA) 
+    cv2.imshow("Video", frame)
 
 video.release()
 cv2.destroyAllWindows()
