f98b8f1cbd80444b70b32eb2b79c1c54d556dbcb
MrYudi <36682691+MrYudi@users.noreply.github.com>
2020-06-06 19:45:52 -0300

    Update camera.py

--- capturaImagem/camera.py
+++ capturaImagem/camera.py
@@ -6,21 +6,21 @@ import os
 # Aperta Escape para sair do programa
 
 # Configuração:
-video = cv2.VideoCapture(0)
-path = 'capturaImagem/Dataset/'
+path = 'capturaImagem/Dataset/' # Caminho do Dataset
 dicionario = { 255: 'NADA',
 65:'A',66:'B',67:'C',68:'D',69:'E',70:'F',
 71:'G',72:'H',73:'I',74:'J',75:'K',76:'L',
 77:'M',78:'N',79:'O',80:'P',81:'Q',82:'R',
 83:'S',84:'T',85:'U',86:'V',87:'W',88:'X',
 89:'Y',90:'Z'}
-limite_frame = 30
-delay_frame = 10
+limite_frame = 30 # Quantas fotos serão feito?
+delay_frame = 10 # Qual é o delay entre as fotos?
 
 if not os.path.isdir(path): 
     os.mkdir(path) # Cria a pasta Dataset, caso nao exista
 
 # Iniciali [não mexer]
+video = cv2.VideoCapture(0)
 count_nome_frame = 0
 count_frame = 30
 count_frame_delay = 0
